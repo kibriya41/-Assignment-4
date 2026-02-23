fba0609af47f99ec389697d46439e894d3894692
kibriya41 <kibriyaamit17@gmail.com>
2026-02-23 23:33:58 +0600

    project almost done

--- js/application.js
+++ js/application.js
@@ -1,6 +1,7 @@
 // console.log("hello");
 let interviewList = [];
 let rejectedList = [];
+let currentStatus = "all";
 
 
 
@@ -19,6 +20,9 @@ const mainSection = document.querySelector("main");
 
 const interviewSection = document.getElementById("interview-section");
 
+const rejectedSection = document.getElementById("rejected-section");
+
+// toggling btn 
  function toggleBtnColor(id){
     console.log(id);
     
@@ -34,7 +38,29 @@ rejectedBtn.classList.add('text-[#64748B]');
 
 // add color 
 const showOnly = document.getElementById(id);
+currentStatus = id;
+
 showOnly.classList.add('bg-[#3B82F6]','text-white');
+
+if(id === "interview-btn"){
+  allCardsSection.classList.add("hidden");
+  rejectedSection.classList.add("hidden");
+  interviewSection.classList.remove("hidden")
+  addIntervewCards();
+}
+else if(id === "rejected-btn"){
+  interviewSection.classList.add("hidden");
+  allCardsSection.classList.add("hidden");
+  rejectedSection.classList.remove("hidden");
+   addrejectedCards();
+  
+}
+else if(id === "all-btn"){
+  allCardsSection.classList.remove("hidden");
+  interviewSection.classList.add("hidden");
+  rejectedSection.classList.add("hidden");
+}
+
  };
 
 //  count 
@@ -47,7 +73,7 @@ applicationCount()
 
 mainSection.addEventListener("click", function(){
     console.log(event.target.classList.contains("interview-btn"));
-    
+  
     if(event.target.classList.contains("interview-btn")){
     const parentNode = event.target.parentNode.parentNode;
     
@@ -57,50 +83,95 @@ mainSection.addEventListener("click", function(){
     const status = parentNode.querySelector(".status-fo-ap").innerText;
     const summary = parentNode.querySelector(".summary").innerText;
 
+    parentNode.querySelector(".status-fo-ap").innerText = "Interview";
+
     const cardInfo = {
         name, 
         skill, 
         salary,
-        status,
+        status:"Interview",
         summary
     }
 
-    const exist = interviewList.find(item => item.name === cardInfo.name);
+    const exist = interviewList.find(item => item.name == cardInfo.name);
 
     if(!exist){
         interviewList.push(cardInfo);
     }
-    addIntervewCades()
+
+    rejectedList = rejectedList.filter(item => item.name != cardInfo.name);
+
+    if(currentStatus = "rejected-btn"){
+      addrejectedCards();
     }
+
+    applicationCount();
+    }
+    else if(event.target.classList.contains("rejected-btn")){
+    const parentNode = event.target.parentNode.parentNode;
     
+    const name = parentNode.querySelector(".name").innerText;
+    const skill = parentNode.querySelector(".skill").innerText;
+    const salary = parentNode.querySelector(".salary").innerText;
+    const status = parentNode.querySelector(".status-fo-ap").innerText;
+    const summary = parentNode.querySelector(".summary").innerText;
+
+    parentNode.querySelector(".status-fo-ap").innerText = "Rejected";
+
+    const cardInfo = {
+        name, 
+        skill, 
+        salary,
+        status:"Rejected",
+        summary
+    }
+
+    const exist = rejectedList.find(item => item.name == cardInfo.name);
+
+    if(!exist){
+        rejectedList.push(cardInfo);
+    }
+
+    interviewList = interviewList.filter(item => item.name != cardInfo.name);
+
+    if(currentStatus == "interview-btn"){
+      addIntervewCards();
+    }
+
+    applicationCount();
+
+    // addrejectedCards();
+
+    }
+     console.log(event.target.classList.contains("rejected-btn"));
 })
 
-function addIntervewCades(){
+// add card on interview sectinon 
+function addIntervewCards(){
     interviewSection.innerHTML = '';
 
     for(let interview of interviewList){
        console.log(interview);
         
        let div = document.createElement("div");
-       div.className = "bg-base-100 border border-gray-200 rounded-lg p-6 flex justify-between"
+       div.className = "bg-base-100 border border-gray-200 rounded-lg p-6 flex justify-between mb-4"
        div.innerHTML = `
         <div>
             <h4 class="name font-bold text-xl text-[#002C5C] mb-1">
-              Modile First Corp
+              ${interview.name}
             </h4>
-            <p class="skill text-[#64748B] font-light mb-5">React Native Developer</p>
+            <p class="skill text-[#64748B] font-light mb-5">${interview.skill}</p>
             <p class="salary text-[#64748B] font-light">
-              Remote • Full-time • $130,000 - $175,000
+             ${interview.salary}
             </p>
             <div class="my-5">
               <p
                 class="status-fo-ap bg-blue-100 text-blue-950 py-2 px-3 rounded-sm w-28 mb-2"
               >
-                Not Applied
+                ${interview.status}
               </p>
               <p class="summary">
-                Build cross-platform mobile applications using React Native.
-                Work on products used by millions of users worldwide.
+                ${interview.summary}
               </p>
             </div>
             <div class="flex gap-2">
@@ -119,5 +190,57 @@ function addIntervewCades(){
             </button>
           </div>
         `
+        interviewSection.appendChild(div);
     } 
 }
+
+// add card on rejected section 
+function addrejectedCards(){
+    rejectedSection.innerHTML = '';
+
+    for(let rejected of rejectedList){
+       console.log(rejected);
+        
+       let div = document.createElement("div");
+       div.className = "bg-base-100 border border-gray-200 rounded-lg p-6 flex justify-between mb-4"
+       div.innerHTML = `
+        <div>
+            <h4 class="name font-bold text-xl text-[#002C5C] mb-1">
+              ${rejected.name}
+            </h4>
+            <p class="skill text-[#64748B] font-light mb-5">${rejected.skill}</p>
+            <p class="salary text-[#64748B] font-light">
+             ${rejected.salary}
+            </p>
+            <div class="my-5">
+              <p
+                class="status-fo-ap bg-blue-100 text-blue-950 py-2 px-3 rounded-sm w-28 mb-2"
+              >
+                ${rejected.status}
+              </p>
+              <p class="summary">
+                ${rejected.summary}
+              </p>
+            </div>
+            <div class="flex gap-2">
+              <button class="interview-btn btn text-green-400 border border-green-400">
+                interview
+              </button>
+              <button class="rejected-btn btn text-red-500 border border-red-500">
+                Rejected
+              </button>
+            </div>
+          </div>
+          <!-- delede btn  -->
+          <div>
+            <button class="btn rounded-full">
+              <i class="fa-regular fa-trash-can"></i>
+            </button>
+          </div>
+        `
+        rejectedSection.appendChild(div);
+    } 
+}
+
+
+
